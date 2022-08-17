76206ebd8970941f1643f413deb956790151c2ca
krisshna666 <krishna199987@gmail.com>
2022-08-17 11:46:27 +0530

    select added ngzorro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('../app/@modules/login/login.module').then((m) => m.LoginModule),
   },
+  {
+    path: '',
+    loadChildren: () =>
+      import('../app/userlist/userlist.module').then((m) => m.UserlistModule),
+  },
 ];
 
 @NgModule({
